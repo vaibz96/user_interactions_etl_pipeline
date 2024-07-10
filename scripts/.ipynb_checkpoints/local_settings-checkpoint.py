d4cd2e9ac363d6d1bef256678345691736f65360
vaibz96 <vshanbha@stevens.edu>
2024-07-10 19:18:55 +0530

    Add ETL scripts

---
+++ scripts/.ipynb_checkpoints/local_settings-checkpoint.py
@@ -0,0 +1,7 @@
+postgresql = {
+            'pguser' : 'postgres',
+            'pgpassword' : 'test3512',
+            'pghost' : 'localhost',
+            'pgport' : 5432,
+            'pgdb' : 'interactionsdb'
+            }
